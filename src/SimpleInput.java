4da78fdfc8ce3e39be12bec6446fa298a4d74180
BatLeDev <guerny.e2200228@etud.univ-ubs.fr>
2023-01-10 19:37:07 +0100

    Ajout class pour ignorer erreur VSCode

---
+++ src/SimpleInput.java
@@ -0,0 +1,16 @@
+// Pour ignorer les erreurs insupportables de VSCode
+public class SimpleInput {
+
+    public static int getInt(String string) {
+        return 0;
+    }
+
+    public static String getString(String string) {
+        return null;
+    }
+
+    public static boolean getBoolean(String string) {
+        return false;
+    }
+
+}
